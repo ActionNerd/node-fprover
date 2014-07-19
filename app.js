b793ae62896821c0057da013d15aa0b121e32444
ActionNerd <john.mastriani@gmail.com>
2014-07-19 12:30:06 +0000

    Made the terrible code slightly less terrible. Experimented with servo range issue.

--- app.js
+++ app.js
@@ -1,89 +1,68 @@
-// **************************** Requires *********************************
 
 var express = require('express');
 var app = express();
 var io = require('socket.io');
 var five = require("johnny-five"),
     board = new five.Board({ port: "/dev/ttyAMA0" });
-var express = require('express');
-var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-var rStickXtrans = 90,
-    rStickYtrans = 90;
-var pinVal = 0;
-
+var rStickXtrans,
+    rStickYtrans,
+	ySlope,
+	xSlope;
 
 // Start express server, serving up static files in the RPi public folder
 app.use(express.static(__dirname + '/public'));
-
 server.listen(3000);
-
 console.log("Listening on Port 3000...");
 
-
-// five.Board().on("ready", function() {
+// Initialize the connection between the node app and the arduino (firmata sketch)
+// Note explicit device path up above "ttyAMA0"; this is for the GPIO serial connection
 board.on("ready", function() {
+
+	// **** declare pin objects
+    // The "pin" portion is just a simple way to test connectivity.
+	// I connected a LED to pin 12 to make sure I had good comms
+	// between RPi and Arduino.  Hitting FACE_3 on the controller
+	// illuminates the LED.
 	var pin = new five.Pin(12);
+	
+	// **** declare servo objects
+	// Initialize the two johnny-five servo instances I'm using.
+	// servox is for the "theta" movement -- the left / right on the camera gimbal.
+	// servoy is for the "phi" movement -- the up / down on the camera gimbal.
 	var servox = new five.Servo({
           pin: 9,
-          center: true,
-          range: [30, 150]
-        });
-        var servoy = new five.Servo({
-          pin: 10,
-          center: true,
-          range: [30, 150]
-        });
-
-	// Initialize and work with open sockets
+          center: true
+    });
+	var servoy = new five.Servo({
+		pin: 10,
+		center: true
+	});
+	
+	// **** SOCKETS SOCKETS SOCKETS SOCKETS SOCKETS ****
 	io.sockets.on('connection', function (socket) {
-		// Emits a hello world to client with open socket to the console
-                //servo.to(rStickXtrans);
-		socket.emit('news', { hello: 'world' });
-		socket.on('my other event', function (data) {
-			console.log(data);
-		});
-			// Listens for button_down and axis_change events
-			// then logs them
+		// **** BUTTONS ****
+		// **** Go to function which handles button pushes
+		
 		socket.on('button_down', function(data) {
 			console.log(data);
-			console.log(data.button);
-			if (data.button == 'FACE_3') {
-				if (pinVal == 0) {
-					console.log("Inside the conditional...");
-					pin.high();
-					pinVal = 1;
-				//	servo.to(90);
-				}
-				else {
-					pin.low();
-					pinVal = 0;
-				//	servo.to(0);
-				}
-				
-				
-			}
 		});
+		
 		socket.on('axis_change', function(data) {
-			console.log(data);
-			rStickXtrans = 90 * data.X + 90; 
-			rStickXtrans = Math.round(rStickXtrans);
+
+			// **** RIGHT STICK ****
+			// **** Go to function which handles camera gimbal movement
 			if (data.stick == 'RIGHT_STICK') {
-				console.log("X-axis data of " + data.X + " equals a degree value of " + rStickXtrans);
-                                servox.to(rStickXtrans);
-                                
-				
+				rStickXtrans = Math.round(90 * data.X + 90);
+				rStickYtrans = Math.round(90 * data.Y + 90);
+				console.log(data.stick + " X-Pos: " + rStickXtrans + "; Y-Pos: " + rStickYtrans); 
+				servox.to(rStickXtrans);
+				servoy.to(rStickYtrans);
 			}
-
-                        rStickYtrans = 90 * data.Y + 90;
-                        rStickYtrans = Math.round(rStickYtrans);
-                        if (data.stick == 'RIGHT_STICK') {
-                                console.log("Y-axis data of " + data.Y + " equals a degree value of " + rStickYtrans);
-                                servoy.to(rStickYtrans);
-
-                        }
-
+			// **** LEFT STICK ****
+			// **** Go to function which handles camera gimbal movement
+			// For the near future -- waiting on motors, chassis, and motorshield
 		}); 
 	});
 });
