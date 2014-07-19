41bf28f1f4857d89a4267c5cd2be813bb9863410
ActionNerd <john.mastriani@gmail.com>
2014-07-19 16:17:49 +0000

    Added a temporary fix for servo degree value translation.

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ var io = require('socket.io').listen(server);
 var rStickXtrans,
     rStickYtrans,
 	ySlope,
-	xSlope;
+	xSlope,
+	yInt,
+	xInt;
 
 // Start express server, serving up static files in the RPi public folder
 app.use(express.static(__dirname + '/public'));
@@ -25,20 +27,29 @@ board.on("ready", function() {
 	// I connected a LED to pin 12 to make sure I had good comms
 	// between RPi and Arduino.  Hitting FACE_3 on the controller
 	// illuminates the LED.
-	var pin = new five.Pin(12);
+	var led = new five.Led(12);
 	
 	// **** declare servo objects
 	// Initialize the two johnny-five servo instances I'm using.
 	// servox is for the "theta" movement -- the left / right on the camera gimbal.
 	// servoy is for the "phi" movement -- the up / down on the camera gimbal.
 	var servox = new five.Servo({
-          pin: 9,
-          center: true
+        pin: 9,
+		range: [37, 135]
     });
 	var servoy = new five.Servo({
 		pin: 10,
-		center: true
+		range: [40, 115]
 	});
+	ySlope = (servoy.range[1] - servoy.range[0])/2;
+	yInt = servoy.range[1] - ySlope;
+	
+	xSlope = (servox.range[1] - servox.range[0])/2;
+	xInt = servox.range[1] - xSlope;
+	
+	// console.log("y-servo slope = " + ySlope + "; y-servo intercept is = " + yInt);
+	// console.log("x-servo slope = " + xSlope + "; x-servo intercept is = " + xInt);
+	
 	
 	// **** SOCKETS SOCKETS SOCKETS SOCKETS SOCKETS ****
 	io.sockets.on('connection', function (socket) {
@@ -47,6 +58,9 @@ board.on("ready", function() {
 		
 		socket.on('button_down', function(data) {
 			console.log(data);
+			if (data.button == 'FACE_3') { 
+				led.toggle();
+			}
 		});
 		
 		socket.on('axis_change', function(data) {
@@ -54,8 +68,8 @@ board.on("ready", function() {
 			// **** RIGHT STICK ****
 			// **** Go to function which handles camera gimbal movement
 			if (data.stick == 'RIGHT_STICK') {
-				rStickXtrans = Math.round(90 * data.X + 90);
-				rStickYtrans = Math.round(90 * data.Y + 90);
+				rStickXtrans = Math.round(xSlope * data.X + xInt);
+				rStickYtrans = Math.round(ySlope * data.Y + yInt);
 				console.log(data.stick + " X-Pos: " + rStickXtrans + "; Y-Pos: " + rStickYtrans); 
 				servox.to(rStickXtrans);
 				servoy.to(rStickYtrans);
