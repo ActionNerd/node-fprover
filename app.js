414da5c059ed22ff0e3c532ab9f6477464af0e7f
ActionNerd <john.mastriani@gmail.com>
2014-07-19 09:19:28 +0000

    Backed up from a known good copy.

--- app.js
+++ app.js
@@ -1,41 +1,5 @@
-
-var express = require('express');
-var app = express();
-var io = require('socket.io');
-
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-
-app.use(express.static(__dirname + '/public'));
-
-server.listen(3000);
-
-console.log("Listening on Port 3000...");
-
-// Initialize and work with open sockets
-io.sockets.on('connection', function (socket) {
-
-        // Emits a hello world to client with open socket
-        // to the console	
-        socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function (data) {
-		console.log(data);
-	});
-        // Listens for button_down and axis_change events
-        // then logs them
-	socket.on('button_down', function(data) {
-		console.log(data);
-	});
-	socket.on('axis_change', function(data) {
-		console.log(data); 
-	});
-	
-});
 // **************************** Requires *********************************
-// Checking to see if this will do anything
-// Checking to see if this will also do anything
+
 var express = require('express');
 var app = express();
 var io = require('socket.io');
@@ -123,3 +87,4 @@ board.on("ready", function() {
 		}); 
 	});
 });
+
